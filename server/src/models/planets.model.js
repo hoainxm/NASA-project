7739aa6259440ca60ab796ee1251641cf0f5dca4
namm0704 <namm0704@gmail.com>
2022-12-17 00:07:07 +0700

    first commit

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,8 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { parse } = require('csv-parse');
 
-
-const habitablePlanets = [];
+const planets = require('./planets.mongo');
 
 function isHabitablePlanet(planet) {
   return planet['koi_disposition'] === 'CONFIRMED'
@@ -12,30 +11,50 @@ function isHabitablePlanet(planet) {
 }
 
 function loadPlanetsData() {
-    return new Promise((resolve, reject) => {
-      fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
       .pipe(parse({
         comment: '#',
         columns: true,
       }))
-      .on('data', (data) => {
+      .on('data', async (data) => {
         if (isHabitablePlanet(data)) {
-          habitablePlanets.push(data);
+          savePlanet(data);
         }
       })
       .on('error', (err) => {
         console.log(err);
         reject(err);
       })
-      .on('end', () => {
-        console.log(`${habitablePlanets.length} habitable planets found!`);
+      .on('end', async () => {
+        const countPlanetsFound = (await getAllPlanets()).length;
+        console.log(`${countPlanetsFound} habitable planets found!`);
         resolve();
       });
   });
 }
 
+async function getAllPlanets() {
+  return await planets.find({}, {
+    '_id': 0, '__v': 0,
+  });
+}
 
-  module.exports = {
-    loadPlanetsData,
-    planets: habitablePlanets,
-  };
+async function savePlanet(planet) {
+  try {
+    await planets.updateOne({
+      keplerName: planet.kepler_name,
+    }, {
+      keplerName: planet.kepler_name,
+    }, {
+      upsert: true,
+    });
+  } catch(err) {
+    console.error(`Could not save planet ${err}`);
+  }
+}
+
+module.exports = {
+  loadPlanetsData,
+  getAllPlanets,
+};
--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 const {
-    getAllPlanets,
+  httpGetAllPlanets,
 } = require('./planets.controller');
 
 const planetsRouter = express.Router();
- 
-planetsRouter.get('/planets', getAllPlanets);
 
-module.exports = planetsRouter; 
+planetsRouter.get('/', httpGetAllPlanets);
+
+module.exports = planetsRouter;
